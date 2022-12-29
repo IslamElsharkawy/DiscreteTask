ab2b5061f3b1a3dc3c24fbd5a15373c663308c5a
IslamElsharkawy <121634605+IslamElsharkawy@users.noreply.github.com>
2022-12-30 00:50:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Project_2
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+int sum;
+
+            Console.WriteLine("Enter n1 : ");
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter n2 : ");
+            int z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The Perfect Numbers between " + i+ " to " + z + " are :");
+
+            for (int n = i; n <= z; n++)
+            {
+                sum = 0;
+                for (int x = 1; x < n; x++)
+                {
+                    if (n % x == 0)
+                    {
+                        sum = sum + x;
+                    }
+                }
+
+                if (sum == n)
+                {
+                    Console.WriteLine(n );
+                }
+            }
+		}
+	}
+}
