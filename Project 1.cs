0f553171ea637078d3e63d8ef862794536092cdb
IslamElsharkawy <121634605+IslamElsharkawy@users.noreply.github.com>
2022-12-30 00:48:40 +0200

    Add files via upload

---
+++ Project 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+    public static class Program 
+    {         
+        public static void Main()
+        {
+            
+            Console.WriteLine("Enter n1"); 
+          
+            int i= int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Enter n2"); 
+            int z= int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Prime numbers from n1 to n2 :"); 
+            int x; 
+            for(i=i;i<=z;i++) 
+            {  
+             for(x=2;x<=i;x++) 
+             { 
+              if(i%x==0) 
+              break; 
+             } 
+             if(i==x) 
+             Console.WriteLine(i);
+            }
+        }
+    }
+}
